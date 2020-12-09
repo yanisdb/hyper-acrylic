0ca5eb87fd5ba50386d768e6c038b2cd3aa96940
De Busschere Yanis <yanis.debusschere@epfl.ch>
2020-12-09 14:56:54 +0100

    Refactoring and house keeping

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports.onWindow = (browserWindow) => {
     const handle = new Uint32Array(browserWindow.getNativeWindowHandle().buffer)[0]
-    require("child_process").spawn(`${__dirname}\\Acrylic.exe`,  [handle, 1]);
+    require("child_process").spawn(`${__dirname}\\acrylic.exe`,  [handle, 1]);
 }

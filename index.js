4fe89a8b57984923a815c1526fecfa7349f1f9b9
De Busschere Yanis <yanis.debusschere@gmail.com>
2020-12-12 16:06:53 +0100

    Option for real acrylic

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-module.exports.onWindow = (browserWindow) => {
-    const handle = new Uint32Array(browserWindow.getNativeWindowHandle().buffer)[0]
-    require("child_process").spawn(`${__dirname}\\acrylic.exe`,  [handle, 1]);
+const defaultConfig = {
+    blur: "acrylic",
+    red: 0,
+    green: 0,
+    blue: 0,
+    alpha: 150
+}
+
+const checkConfig = (config, setting) => {
+    return config.hasOwnProperty("acrylic") && config.acrylic[setting] !== undefined
+    ? config.acrylic[setting]
+    : defaultConfig[setting];
+}
+
+exports.decorateConfig = config => {
+    exports.onWindow = (browserWindow) => {
+        browserWindow.setBackgroundColor("00000000");
+        const handle = new Uint32Array(browserWindow.getNativeWindowHandle().buffer)[0]
+        let state = -1;
+        if (checkConfig(config, "blur") == "acrylic") state = 2;
+        if (checkConfig(config, "blur") == "classic") state = 1;
+        require("child_process").spawn(`${__dirname}\\acrylic.exe`,  
+            [
+                handle, 
+                state, 
+                checkConfig(config, "red"),
+                checkConfig(config, "green"),
+                checkConfig(config, "blue"), 
+                checkConfig(config, "alpha")
+            ]
+        );
+    }
+    return config;
 }
--- src/Acrylic.cpp
+++ src/Acrylic.cpp
@@ -12,10 +12,21 @@ struct WINCOMPATTRDATA {
 	ULONG ulDataSize;
 };
 
+/// <summary>
+/// 
+/// </summary>
+/// <param name="argv[1]"> The handle of the window</param>
+/// <param name="argv[2]"> The type of blur, i.e., 0 for transparent blur and 1 for acrylic</param>
+/// <param name="argv[3]"> The red value</param>
+/// <param name="argv[4]"> The green value</param>
+/// <param name="argv[5]"> The bluevalue</param>
+/// <param name="argv[6]"> The alpha value</param>
+/// <returns></returns>
 int main(int argc, const char* argv[])
 {
 	HWND hwnd = (HWND)atoi(argv[1]);
-	boolean enable = argc < 2 || (argc >= 2 && argv[2] != "false");
+	int state = atoi(argv[2]) + 2;
+	int color = atoi(argv[3]) | (atoi(argv[4]) << 8) | (atoi(argv[5]) << 16) | (atoi(argv[6]) << 24);
 
 	const HINSTANCE hModule = LoadLibrary(TEXT("user32.dll"));
 	if (hModule) {
@@ -23,7 +34,7 @@ int main(int argc, const char* argv[])
 		const pSetWindowCompositionAttribute
 			SetWindowCompositionAttribute = (pSetWindowCompositionAttribute)GetProcAddress(hModule, "SetWindowCompositionAttribute");
 		if (SetWindowCompositionAttribute) {
-			ACCENTPOLICY policy = { enable ? 3 : 0 , 0, 0, 0 };
+			ACCENTPOLICY policy = { state, 0, color , 0 };
 			WINCOMPATTRDATA data = { 19, &policy, sizeof(ACCENTPOLICY) };
 			SetWindowCompositionAttribute(hwnd, &data);
 		}
